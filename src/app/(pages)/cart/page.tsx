110e245e0152bc0e9e6e899d89ef60c8960e3232
itseskimo <ayushmatmotgill@gmail.com>
2024-02-22 15:22:43 +0530

    bug:fix deployment err

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -3,8 +3,8 @@ import PaymentSection from '@/components/paymentSection/paymentSection'
 import CheckoutItems from '@/components/checkoutItems/checkoutItems'
 import Navbar from '@/components/navbar/navbar'
 import { getServerSession } from "next-auth/next"
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-
+// import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/app/lib/auth'
 const page = async () => {
 
     const session = await getServerSession(authOptions);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Navbar from '@/components/navbar/navbar'
 import { getProducts } from '@/config/actions/actions'
 import ProductItems from '@/components/productItems/productItems'
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { authOptions } from '@/app/lib/auth'
 
 const Home = async () => {
   const allProducts = await getProducts()
